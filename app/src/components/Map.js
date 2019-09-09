ce523c86f43cccce641e414885e8cd678aefbe47
Sam Noh <samnoh21@outlook.com>
2019-09-09 15:16:25 +1200

    Add MapView

---
+++ app/src/components/Map.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import MapView, { Polyline } from 'react-native-maps';
+
+const styles = StyleSheet.create({
+    map: {
+        height: 300
+    }
+});
+
+const Map = () => {
+    return (
+        <MapView
+            style={styles.map}
+            initialRegion={{
+                latitude: 37.33233,
+                longitude: -122.03121,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01
+            }}
+        >
+            <Polyline />
+        </MapView>
+    );
+};
+
+export default Map;
--- app/src/screens/TrackCreateScreen.js
+++ app/src/screens/TrackCreateScreen.js
@@ -1,10 +1,35 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import { requestPermissionsAsync } from 'expo-location';
+
+import Map from '../components/Map';
 
 const styles = StyleSheet.create({});
 
 const TrackCreateScreen = () => {
-    return <View></View>;
+    const [error, setError] = useState(null);
+
+    const startWatching = async () => {
+        try {
+            await requestPermissionsAsync();
+        } catch (e) {
+            setError(e);
+        }
+    };
+
+    useEffect(() => {
+        startWatching();
+    }, []);
+
+    return (
+        <SafeAreaView forceInse={{ top: 'always' }}>
+            <Text h3>Create a Track</Text>
+            <Map />
+            {error ? <Text>Please enable location services</Text> : null}
+        </SafeAreaView>
+    );
 };
 
 export default TrackCreateScreen;
